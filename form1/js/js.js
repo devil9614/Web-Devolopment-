dc9f966413eec52be05591dfa32c20282d405674
Sujan <sujanmukherjee9614@gmail.com>
2021-01-13 12:44:19 +0530

    Little change has been done

--- form1/js/js.js
+++ form1/js/js.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-
+    function refresh() {
+        window.location.reload();
+    }
+    $("createYourOwn").hide(2000, function() {
+        $("createYourOwn").show(2000);
+    });
 })

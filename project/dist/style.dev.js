0dfd0c5b2ae4e5e922aa11edf2dff855c875a5e6
Sujan Mukherjee <44901151+devil9614@users.noreply.github.com>
2021-05-10 10:50:23 +0530

    client work

---
+++ project/dist/style.dev.js
@@ -0,0 +1,12 @@
+"use strict";
+
+$(document).ready(function () {
+  // Toggle menu on click
+  $("#menu-toggler").click(function () {
+    toggleBodyClass("menu-active");
+  });
+
+  function toggleBodyClass(className) {
+    document.body.classList.toggle(className);
+  }
+});
